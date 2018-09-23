ddee25a1ce96aa4df627c330353fd208b5b64caa
Renard <renard.mbm@yandex.ru>
2018-09-23 15:37:45 +1000

    Add lesson4

---
+++ lesson4/phoneNumber.py
@@ -0,0 +1,29 @@
+def checkPhoneNumber(phone_number):
+    phoneNumber = ''.join(phone_number.split())
+    if phoneNumber[0] == '8':
+        phoneNumber = phoneNumber[1:]
+    elif phoneNumber[0] == '+' and phoneNumber[1] == '7':
+        phoneNumber = phoneNumber[2:]
+    else:
+        return 'error'
+
+    if phoneNumber.count('(') == phoneNumber.count(')'):
+        correctPhoneNumber = []
+        isLastMinus = False
+        for symbol in phoneNumber:
+            if symbol.isdigit():
+                correctPhoneNumber.append(symbol)
+                isLastMinus = False
+            elif symbol == '-' and not isLastMinus:
+                isLastMinus = True
+                continue
+            elif symbol == '(' or symbol == ')':
+                continue
+            else:
+                return 'error'
+        return '+7' + ''.join(correctPhoneNumber)
+    else:
+        return 'error'
+
+
+print(checkPhoneNumber(input()))
