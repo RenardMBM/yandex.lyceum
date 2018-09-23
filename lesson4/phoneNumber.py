2000fb4ae6bdf4ff9406b63a77139021278ba72f
Renard <renard.mbm@yandex.ru>
2018-09-23 17:48:19 +1000

    fixed errors

--- lesson4/phoneNumber.py
+++ lesson4/phoneNumber.py
@@ -2,12 +2,16 @@ def checkPhoneNumber(phone_number):
     phoneNumber = ''.join(phone_number.split())
     if phoneNumber[0] == '8':
         phoneNumber = phoneNumber[1:]
+
     elif phoneNumber[0] == '+' and phoneNumber[1] == '7':
         phoneNumber = phoneNumber[2:]
+
     else:
         return 'error'
 
-    if phoneNumber.count('(') == phoneNumber.count(')'):
+    openCount, closeCount = phoneNumber.count('('), phoneNumber.count(')')
+
+    if openCount == closeCount and (openCount == 1 or closeCount == 0):
         correctPhoneNumber = []
         isLastMinus = False
 
@@ -15,15 +19,20 @@ def checkPhoneNumber(phone_number):
             if symbol.isdigit():
                 correctPhoneNumber.append(symbol)
                 isLastMinus = False
+
             elif symbol == '-' and not isLastMinus:
                 isLastMinus = True
                 continue
+
             elif symbol == '(' or symbol == ')':
                 continue
+
             else:
                 return 'error'
 
-        return '+7' + ''.join(correctPhoneNumber)
+        return '+7' + ''.join(correctPhoneNumber) \
+            if not isLastMinus and len(correctPhoneNumber) == 10 else 'error'
+
     else:
         return 'error'
 
