aaf28d2c7eb1e1e5630598a928c6821cd80c466b
renard <renard.mbm@yandex.ru>
2018-09-28 00:29:21 +1000

    thirdVersion Password

--- lesson5/Password.py
+++ lesson5/Password.py
@@ -1,65 +1,95 @@
-def main():
-    def checkTrio(parole: str):
-        for indexOfLetter in range(1, len(parole) - 1):
-            eng = 'qwertyuiopasdfghjklzxcvbnm------'
-            rus = 'йцукенгшщзхъфывапролджэячсмитьбю'
-            preferLetter = parole[indexOfLetter - 1]
-            nextLetter = parole[indexOfLetter + 1]
-            indexOfNextLetter = 0
-            indexOfPreferLetter = 0
+class PasswordError(Exception):
+    pass
 
-            if preferLetter.isdigit() or nextLetter.isdigit() or parole[indexOfLetter].isdigit():
-                continue
 
-            if 'ё' == preferLetter:
-                indexOfPreferLetter = 23
-                indexOfLetter += 1 if indexOfLetter < 23 else 0
-                indexOfNextLetter += 1 if indexOfNextLetter < 23 else 0
+class LengthError(PasswordError):
+    pass
 
-            if 'ё' == nextLetter:
-                indexOfNextLetter = 23
-                indexOfLetter += 1 if indexOfLetter < 23 else 0
-                indexOfPreferLetter += 1 if indexOfNextLetter < 23 else 0
 
-            if 'ё' == parole[indexOfLetter]:
-                indexOfLetter = 23
-                indexOfPreferLetter += 1 if indexOfPreferLetter < 23 else 0
-                indexOfNextLetter += 1 if indexOfNextLetter < 23 else 0
+class LetterError(PasswordError):
+    pass
 
-            indexOfPreferLetter += eng.find(preferLetter) if preferLetter in eng else \
-                rus.find(preferLetter) if preferLetter in rus else 0
-            indexOfNextLetter += eng.find(nextLetter) if nextLetter in eng else \
-                rus.find(nextLetter) if nextLetter in rus else 0
-            indexes = sorted([indexOfNextLetter, indexOfLetter, indexOfPreferLetter])
 
-            if indexes[0] + 1 == indexes[1] == indexes[2] - 1:
-                return False
+class DigitError(PasswordError):
+    pass
 
-        return True
 
-    def checkPassword(password):
-        if len(password) >= 8:
-            isUpper, isLower, isDigit = False, False, False
+class SequenceError(PasswordError):
+    pass
 
-            for letter in password:
-                if letter.isdigit():
-                    isDigit = True
 
-                elif letter.isupper():
-                    isUpper = True
+def checkTrio(parole: str):
+    for indexOfLetter in range(1, len(parole) - 1):
+        eng = 'qwertyuiopasdfghjklzxcvbnm------'
+        rus = 'йцукенгшщзхъфывапролджэячсмитьбю'
+        preferLetter = parole[indexOfLetter - 1]
+        letter = parole[indexOfLetter]
+        nextLetter = parole[indexOfLetter + 1]
+        indexOfLetter = 0
+        indexOfNextLetter = 0
+        indexOfPreferLetter = 0
 
-                elif letter.islower():
-                    isLower = True
+        if preferLetter.isdigit() or nextLetter.isdigit() or letter.isdigit():
+            continue
 
-            return 'ok' if (isDigit and
-                            isUpper and
-                            isLower and
-                            checkTrio(password.lower())) else 'error'
+        if 'ё' == preferLetter:
+            indexOfPreferLetter = 23
+            indexOfLetter += 1 if indexOfLetter < 23 else 0
+            indexOfNextLetter += 1 if indexOfNextLetter < 23 else 0
+
+        if 'ё' == nextLetter:
+            indexOfNextLetter = 23
+            indexOfLetter += 1 if indexOfLetter < 23 else 0
+            indexOfPreferLetter += 1 if indexOfNextLetter < 23 else 0
+
+        if 'ё' == letter:
+            indexOfLetter = 23
+            indexOfPreferLetter += 1 if indexOfPreferLetter < 23 else 0
+            indexOfNextLetter += 1 if indexOfNextLetter < 23 else 0
+
+        indexOfLetter += eng.find(letter) if letter in eng else rus.find(letter)
+        indexOfPreferLetter += eng.find(preferLetter) if preferLetter in eng else\
+            rus.find(preferLetter)
+        indexOfNextLetter += eng.find(nextLetter) if nextLetter in eng else rus.find(nextLetter)
+        indexes = sorted([indexOfNextLetter, indexOfLetter, indexOfPreferLetter])
+
+        if indexes[0] + 1 == indexes[1] == indexes[2] - 1:
+            return False
+
+    return True
+
+
+def check_password(password):
+    if len(password) >= 8:
+        isUpper, isLower, isDigit = False, False, False
+
+        for letter in password:
+            if letter.isdigit():
+                isDigit = True
+
+            elif letter.isupper():
+                isUpper = True
+
+            elif letter.islower():
+                isLower = True
+
+        if not isDigit:
+            raise DigitError
+
+        elif not isUpper or not isLower:
+            raise LetterError
+
+        elif not checkTrio(password.lower()):
+            raise SequenceError
 
         else:
-            return 'error'
+            print('ok')
 
-    print(checkPassword(input()))
+    else:
+        raise LengthError
 
 
-main()
+try:
+    check_password('FсЫнпblIhЩШY2Е9Пг2Qhjk')
+except LetterError or LengthError or SequenceError or DigitError:
+    print('error')
