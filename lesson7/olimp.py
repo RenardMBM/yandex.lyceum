e0e7d1b8aa04b0cb7c6461a400e333d65c6238d1
renard <renard.mbm@yandex.ru>
2018-10-05 14:29:04 +1000

    incorrect output

--- lesson7/olimp.py
+++ lesson7/olimp.py
@@ -1,4 +1,43 @@
 import csv
 
 with open('rez.csv', encoding='utf8') as res:
-    reader = csv.reader(res, delimiter=',', quotechar='')
+    a = []
+    schools = {}
+    reader = csv.reader(res, delimiter=',', quotechar='"')
+
+    for index, row in enumerate(reader):
+        if not row:
+            continue
+
+        r = row[2].split('-')
+
+        if r[2] in schools:
+
+            if r[3] in schools[r[2]]:
+                schools[r[2]][r[3]].append((row[1], row[-1]))
+
+            else:
+                schools[r[2]][r[3]] = [(row[1], row[-1])]
+
+        else:
+            schools[r[2]] = {r[3]: [(row[1], row[-1])]}
+
+    nSchool, nRoom = input().split()
+
+    if nSchool in schools:
+
+        if nRoom in schools[nSchool]:
+            correctP = sorted(schools[nSchool][nRoom], key=lambda x: (
+                x[1], -int(''.join([
+                    str(ord(_)) for _ in x[0]
+                ]))
+            ))
+
+            for p in correctP:
+                print(*p)
+
+        else:
+            print('error incorrect number of room')
+
+    else:
+        print('error incorrect number of school')
